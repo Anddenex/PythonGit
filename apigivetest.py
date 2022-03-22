0288d8da75273211a0c85f0f36ed084c3ab6ecb1
Andrew <andrewpowerant@gmail.com>
2022-03-22 11:30:01 -0600

    Removed code not needed right now

--- apigivetest.py
+++ apigivetest.py
@@ -1,51 +1,6 @@
 from urllib import response
 import requests
 
-# The code below has some code commented out that I tried different options and the code below is the one that is active.
-
-# def givecharityurls():
-#     urls = [
-#         "https://giveblockchain.io/",
-#         "https://giveblockchain.io/give-information/",
-#         "https://giveblockchain.io/mission/",
-#         "https://giveblockchain.io/give-node-information/",
-#         "https://giveblockchain.io/give-app-information/",
-#         "https://giveblockchain.io/lets-give/",
-#         "https://giveblockchain.io/login/",
-#         "https://twitter.com/giveblockchain_",
-#         "https://www.facebook.com/giveblockchain",
-#         "https://www.instagram.com/give_blockchain/",
-#         "https://giveblockchain.io/givers-information/",
-#         "https://giveblockchain.io/save-information/",
-#         "https://giveblockchain.io/water-information/",
-#         "https://giveblockchain.io/humanitarian-information/",   
-#         "https://giveblockchain.io/submit-a-proposal/",
-#         "https://giveblockchain.io/mission-information/",  
-#         "https://giveblockchain.io/privacy-policy/",
-#         "https://giveblockchain.io/terms-of-use/", 
-#         "https://giveblockchain.io/give-to-give/",
-#         "https://giveblockchain.io/all-give-products/",
-#     ]
-
-#     for url in urls:
-
-#         response = requests.get(url)
-#         # print(f'The response code for {url} is {response.status_code}')
-
-#         if response.status_code == 200:
-#             count_url = " "  
-#             count_url = count_url.count(' ')  
-#             count_url = count_url
-#             url_count = count_url + 1
-#             print(f"The response code for {url} is {response.status_code} {'.'*30} OK")
-#             print(f'The number of 200 status codes URLs is: {url_count}')
-
-#         else:
-#             print(f"The url: {url} is {response.status_code} {'.'*30} borked")
-
-
-# givecharityurls()
-
 
 def giveurls():
     urls = [
