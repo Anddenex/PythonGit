e3332487c576cc885cea0d40b762e6ca2826707d
Andrew <andrewpowerant@gmail.com>
2022-05-04 13:33:35 -0600

    Pythong Updates

---
+++ Chapter 2/chapterThree.py
@@ -0,0 +1,18 @@
+# A program to calculate the value of coin into dollars
+from functools import total_ordering
+
+
+def coin_dollars():
+    print("Change Counter")
+    print()
+    print("Please entr the count of each coin type.")
+    quarters = eval(input("How many quarters? "))
+    dimes = eval(input("How many dimes? "))
+    nickles = eval(input("How many nickles? "))
+    pennies = eval(input("How many pennies? "))
+    total = (quarters*.25) + (dimes*.10) + (nickles*.05) + (pennies*.01)
+
+    print(f"The total dollar amount is: ${total:,.2f}")
+
+
+coin_dollars()
---
+++ PythonBook/chapterFive.py
@@ -0,0 +1,17 @@
+# def introduction():
+#     firstName = input("What is your first name? ")
+#     lastName = input("What is your last name? ")
+#     print(f"Hello, {firstName} {lastName}!")
+
+
+# introduction()
+
+
+def introduction2():
+    firstName, lastName = input("What is your first and last name separated by a comma: ")
+    print(f"Hello, {firstName}, {lastName}!")
+    print({firstName})
+    print({lastName})
+
+
+introduction2()
