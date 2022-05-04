2453d4259c8b45b3f2521be6e625da967eb621ad
Andrew <andrewpowerant@gmail.com>
2022-05-04 16:41:21 -0600

    First update to Give page when logged out

---
+++ Cypress/cypress/integration/Give WordPress Automation/give_give_page_logout.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+// When a customer is logged out
+describe('Login', () => {
+    beforeEach(() => {
+        cy.visit("https://giveblockchain.io/give-information/")
+        cy.url()
+            .should("include", "/give-information/")
+       // cy.percySnapshot()
+        cy.get('h1').should(($h1) => {
+            expect($h1, 'H1 Title').to.contain('Give')
+            expect($h1, 'H1 Title').to.have.attr('style', "text-align: center")
+          })    
+    })
+
+    it('Expecting/Asserting text for button accurate', () => {
+        cy.get('h5')
+            .contains("The Give Blockchain is a global community of philanthropists and concerned citizens who share one unifying vision: a community that empowers and provides support to many important causes around the world.")
+            .should('have.attr', 'style', "text-align: center;")
+        cy.get('h4')
+            .should('have.text', "Login to make purchases and give.")
+            .should('have.attr', 'style', "text-align: center;")
+        cy.get('[class="nectar-button jumbo regular regular-button"]').should(($necton) => {
+            //expect($necton).to.have.length(3)
+            expect($necton.eq(0), 'first button')
+                .to.contain('Login')
+                .to.have.attr('style', "margin-right: 10px; margin-left: 10px; color: rgb(239, 61, 57); background-color: rgb(255, 255, 255); visibility: visible;")
+                .to.have.attr('href', "/login/")
+            expect($necton.eq(1), 'second button')
+                .to.contain('Sign Up Free')
+                .to.have.attr('style', "margin-right: 10px; margin-left: 10px; color: rgb(239, 61, 57); background-color: rgb(255, 255, 255); visibility: visible;")
+                .to.have.attr('href', "/lets-give/")
+            })   
+    })
+})
