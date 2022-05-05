197e52d5883990dd00c670789405e51c01150bc9
Andrew <andrewpowerant@gmail.com>
2022-05-05 12:17:27 -0600

    Updates to coins(App)  and loggedout(WP)

--- Cypress/cypress/integration/Give Desktop Automation/give_prices_coin.spec.js
+++ Cypress/cypress/integration/Give Desktop Automation/give_prices_coin.spec.js
@@ -7,12 +7,33 @@ describe('Sidebar Tests', () => {
         cy.wait(1000)
       })
 
-    it('Check the first 10 Coin pages', () => {
-        cy.get('[class="v-responsive__content"]')
-            .eq(0)
-            .click()
-        cy.url().should("include", "/prices/BTC");
-        cy.get('.offset-3 > .container > .row > :nth-child(2)')
-            .should('have.text', "Bitcoin")
+    it('Expecting/Asserting coin is accurate with styling', () => {
+        //cy.percySnapshot()
+        cy.get('[class="v-responsive__content"]').should(($coin) => {
+            expect($coin.eq(0), 'first button')
+                .to.contain("Bitcoin")
+                .to.have.attr('style', "width: 300px;")
+            expect($coin.eq(1), 'second button')
+                .to.contain("Ethereum")
+                .to.have.attr('style', "width: 300px;")
+            expect($coin.eq(2), 'third button')
+                .to.contain("XRP")
+                .to.have.attr('style', "width: 300px;")
+            expect($coin.eq(3), 'fourth button')
+                .to.contain("Bitcoin Cash")
+                .to.have.attr('style', "width: 300px;")
+            expect($coin.eq(4), 'fifth button')
+                .to.contain("Litecoin")
+                .to.have.attr('style', "width: 300px;")
+          })    
     })
+
+    // it('Check the first 10 Coin pages', () => {
+    //     cy.get('[class="v-responsive__content"]')
+    //         .eq(0)
+    //         .click()
+    //     cy.url().should("include", "/prices/BTC");
+    //     cy.get('.offset-3 > .container > .row > :nth-child(2)')
+    //         .should('have.text', "Bitcoin")
+    // })
 })
