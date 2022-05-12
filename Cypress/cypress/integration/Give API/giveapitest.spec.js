b9b08a361f9b774ecfd1e25e158157446ebc5ce7
Andrew <andrewpowerant@gmail.com>
2022-05-12 12:14:23 -0600

    Updates to API test

--- Cypress/cypress/integration/Give API/giveapitest.spec.js
+++ Cypress/cypress/integration/Give API/giveapitest.spec.js
@@ -1,1 +1,10 @@
+/// <reference types="cypress" />
 
+describe('API testing', () => {
+    
+    it('Get -- read', () => {
+        cy.request('GET', 'https://giveblockchain.io/').then((response) => {
+            expect(response).to.have.property('status', 200)
+        })
+    })
+})
