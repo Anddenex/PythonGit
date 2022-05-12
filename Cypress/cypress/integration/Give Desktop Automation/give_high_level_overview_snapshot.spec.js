db97b36829890981a531a6a548302d04453c4273
Andrew <andrewpowerant@gmail.com>
2022-05-12 10:16:13 -0600

    Updates to high level overview to fix errors

--- Cypress/cypress/integration/Give Desktop Automation/give_high_level_overview_snapshot.spec.js
+++ Cypress/cypress/integration/Give Desktop Automation/give_high_level_overview_snapshot.spec.js
@@ -74,87 +74,110 @@ describe('Login', () => {
 
     })
 
-    it('Login Form Elements', () => {
+    it('Nav Menu Content Assertions', () => {
+        cy.login(Cypress.env('email'), Cypress.env('password'));
+        cy.url().should('include', "app.giveblockchain.io")
+        cy.get('[class="v-btn__content"]').should(($navmenu) => {
+            expect($navmenu.eq(0), 'Dashboard Menu Item')
+                .to.contain("Dashboard")
+            expect($navmenu.eq(1), 'Summary Menu Item')
+                .to.contain('summary')
+            expect($navmenu.eq(2), 'Inventory Menu Item')
+                .to.contain('Inventory')
+            expect($navmenu.eq(3), 'Nodes Menu Item')
+                .to.contain('Nodes')
+            expect($navmenu.eq(4), 'Vault Menu Item')
+                .to.contain('Vault')
+            })  
+        cy.get('[class="v-btn v-btn--depressed v-btn--flat v-btn--has-bg v-btn--router v-btn--tile theme--light v-size--default topBar-Nav px-4 transparent black--text"]').should(($navmenu) => {
+            expect($navmenu.eq(0), 'Inventory Menu Item')
+                .to.have.attr('href', '/inventory')
+            expect($navmenu.eq(1), 'Nodes Menu Item')
+                .to.have.attr('href', '/nodes')
+            expect($navmenu.eq(2), 'Vault Menu Item')
+                .to.have.attr('href', '/vault')
+            })    
+        cy.get('[data-cy="nav-summary"]')
+            .should('have.attr', 'href', '/')
+
+    })
+
+    it('Nav Menu Click and Page Assertion', () => {
     // Login to the App
         cy.login(Cypress.env('email'), Cypress.env('password'));
-        cy.wait(5000);
+
         // Checking the Inventory Nav
         cy.get("[data-cy='nav-inventory']").click();
-        cy.get("[class='v-card__title'] ").should($balance_title => {
-            console.log($balance_title.text());
-            expect($balance_title.text()).to.contain("Balances");
-        })
-        cy.wait(5000);
+        cy.get('[class="v-card__title"]').should(($navinvent) => {
+            //expect($necton).to.have.length(3)
+            expect($navinvent.eq(0), 'The Inventory page title contains: ')
+                .to.contain('Balances')
+            })   
+
 
         // Checking the Nodes Nav
         cy.get("[data-cy='nav-nodes']").click();
-        cy.get("a[data-cy='nav-nodes'] span[class='v-btn__content']").should($nodemenu => {
-            console.log($nodemenu.text());
-            expect($nodemenu.text()).to.contain("Nodes");
+        cy.get('[class="v-card__title text-capitalize"]').should(($navnodes) => {
+            expect($navnodes.eq(0), "The Nodes page title contains: ")
+                .to.contain("give Node Info")
         })
-        cy.wait(5000);
 
-        cy.eyesOpen({
-            appName: "Nodes", 
-            testName: "Nodes page snapshot layout check",
-            //browser: { width: 1269, height: 718, name: "firefox"}, Should render a FF image
-            //browser: { width: 1269, height: 718, name: "chrome"}, Should render a chrome image
-            //You can specify other browsers that are compatible with Cypress
+
+        // cy.eyesOpen({
+        //     appName: "Nodes", 
+        //     testName: "Nodes page snapshot layout check",
+        //     //browser: { width: 1269, height: 718, name: "firefox"}, Should render a FF image
+        //     //browser: { width: 1269, height: 718, name: "chrome"}, Should render a chrome image
+        //     //You can specify other browsers that are compatible with Cypress
      
-        });
+        // });
 
-        cy.eyesCheckWindow("Nodes Page");
-        cy.eyesClose();
-        cy.wait(1000);
+        // cy.eyesCheckWindow("Nodes Page");
+        // cy.eyesClose();
+        // cy.wait(1000);
 
         // Checking the Vault Nav
-        cy.get("[data-cy='nav-vault']").click();
-        cy.get("a[data-cy='nav-vault'] span[class='v-btn__content']").should($vaultmenu => {
-            console.log($vaultmenu.text());
-            expect($vaultmenu.text()).to.contain("Vault");
+        cy.get("[data-cy='nav-vault']")
+            .click()
+        cy.get('.v-card__title').should(($navvault) => {
+            expect($navvault, "The Vault page title contains: ")
+                .to.contain("Vault Items")
         })
-        cy.wait(5000);
 
-        // Checking the Summary Nav
-        cy.get("[data-cy='nav-summary']").click();
-        cy.get("a[data-cy='nav-summary'] span[class='v-btn__content']").should($summarymenu => {
-            console.log($summarymenu.text());
-            expect($summarymenu.text()).to.contain("summary");
-        })
-        cy.wait(5000);
-        cy.eyesOpen({
-            appName: "Summary", 
-            testName: "Summary page snapshot layout check",
-            //browser: { width: 1269, height: 718, name: "firefox"}, Should render a FF image
-            //browser: { width: 1269, height: 718, name: "chrome"}, Should render a chrome image
-            //You can specify other browsers that are compatible with Cypress
+        // cy.wait(5000);
+
+        // cy.eyesOpen({
+        //     appName: "Summary", 
+        //     testName: "Summary page snapshot layout check",
+        //     //browser: { width: 1269, height: 718, name: "firefox"}, Should render a FF image
+        //     //browser: { width: 1269, height: 718, name: "chrome"}, Should render a chrome image
+        //     //You can specify other browsers that are compatible with Cypress
      
-        });
+        // });
 
-        cy.eyesCheckWindow("Summary Page");
-        cy.eyesClose();
-        cy.wait(1000);
+        // cy.eyesCheckWindow("Summary Page");
+        // cy.eyesClose();
+        // cy.wait(1000);
 
-        // Checking the Dashboard Nav
-        cy.get("a[class='v-btn v-btn--depressed v-btn--flat v-btn--has-bg v-btn--tile theme--light v-size--default topBar-Nav px-4 transparent black--text'] span[class='v-btn__content']").should($dashboardmenu => {
-            console.log($dashboardmenu.text());
-            expect($dashboardmenu.text()).to.contain("Dashboard");
-        })
-        cy.get("a[class='v-btn v-btn--depressed v-btn--flat v-btn--has-bg v-btn--tile theme--light v-size--default topBar-Nav px-4 transparent black--text'] span[class='v-btn__content']").click();
+        // // Checking the Dashboard Nav
+        cy.get('.v-toolbar__items > [href="https://giveblockchain.io/dashboard"] > .v-btn__content')
+            .click();
+        cy.get('h3')
+            .should('have.text', "Notice")
 
-        cy.wait(5000);
-        cy.eyesOpen({
-            appName: "Dashboard", 
-            testName: "Dashboard page snapshot layout check",
-            //browser: { width: 1269, height: 718, name: "firefox"}, Should render a FF image
-            //browser: { width: 1269, height: 718, name: "chrome"}, Should render a chrome image
-            //You can specify other browsers that are compatible with Cypress
+        // cy.wait(5000);
+        // cy.eyesOpen({
+        //     appName: "Dashboard", 
+        //     testName: "Dashboard page snapshot layout check",
+        //     //browser: { width: 1269, height: 718, name: "firefox"}, Should render a FF image
+        //     //browser: { width: 1269, height: 718, name: "chrome"}, Should render a chrome image
+        //     //You can specify other browsers that are compatible with Cypress
      
-        });
+        // });
 
-        cy.eyesCheckWindow("Dashboard Page");
-        cy.eyesClose();
-        cy.wait(1000);
+        // cy.eyesCheckWindow("Dashboard Page");
+        // cy.eyesClose();
+        // cy.wait(1000);
 
     })
 })
