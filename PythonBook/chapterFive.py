be9eaf05f3cbee2b09f28e91b1b20e1b29e38d6b
Andrew <andrewpowerant@gmail.com>
2022-05-17 09:23:46 -0600

    Update to Python chapter five

--- PythonBook/chapterFive.py
+++ PythonBook/chapterFive.py
@@ -1,21 +1,21 @@
-# def introduction():
-#     firstName = input("What is your first name? ")
-#     lastName = input("What is your last name? ")
-#     print(f"Hello, {firstName} {lastName}!")
+def introduction():
+    firstName = input("What is your first name? ")
+    lastName = input("What is your last name? ")
+    print(f"Hello, {firstName} {lastName}!")
 
 
-# introduction()
+introduction()
 
-# # Receiving error "ValueError: too many values to unpack (expected 2)" and unable to figure it out, works with numeric variables
-# # But not a string
-# def introduction2():
-#     firstName, lastName = input("What is your first and last name separated by a comma: ")
-#     print(f"Hello, {firstName}, {lastName}!")
-#     print({firstName})
-#     print({lastName})
+# Receiving error "ValueError: too many values to unpack (expected 2)" and unable to figure it out, works with numeric variables
+# But not a string
+def introduction2():
+    firstName, lastName = input("What is your first and last name separated by a comma: ")
+    print(f"Hello, {firstName}, {lastName}!")
+    print({firstName})
+    print({lastName})
 
 
-# introduction2()
+introduction2()
 
 #Example lessons for indexing and slicing strings
 def example_indexes_slices():
@@ -43,3 +43,31 @@ def example_rep_length():
 
 example_rep_length()
 
+def str_op():
+    for ch in "Spam!":
+        print(ch, end=" - ")
+
+
+str_op()
+
+# This program will create a computer generated username
+def username():
+    first_name = input("What is your first name: ")
+    middle_name = input("What is your middle name: ")
+    last_name = input("What is your last name: ")
+    uname = first_name[0] + last_name[:6]
+    u_name = first_name[:3] + middle_name[:3] + last_name[:3]
+    print(f"Your username is: {uname.lower()}")
+    print(f"Your username is: {u_name.lower()}")
+
+
+username()
+
+def month_choice():
+    month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    n = eval(input("Enter a month number (1-12): "))
+    month_number = month[n-1]
+    print(month_number)
+
+
+month_choice()
